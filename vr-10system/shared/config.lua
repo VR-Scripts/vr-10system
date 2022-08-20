e20920ce1535ab43f09f70c0ec35745351347b66
RoiNiss <87337416+RoiNiss@users.noreply.github.com>
2022-08-20 16:00:40 +0300

    Update config.lua

--- vr-10system/shared/config.lua
+++ vr-10system/shared/config.lua
@@ -1,8 +1,8 @@
 VRConfig = {}
 
 VRConfig.NewCore = true -- if you are using the exports versin set true
-VRConfig.Core = "WPCore" -- Your core name
-VRConfig.Exports = "wp-core" -- Your core script name
+VRConfig.Core = "QBCore" -- Your core name
+VRConfig.Exports = "qb-core" -- Your core script name
 
 VRConfig.WhitelistedJobs = { --add job for adding an employee list for the current job and pick how to sort it
     -- Example:
@@ -15,7 +15,7 @@ VRConfig.WhitelistedJobs = { --add job for adding an employee list for the curre
     ['police'] = {
         ['SortBy'] = 'tag'--how to sort players on the list.
     },
-    ['vu'] = {
+    ['ambulance'] = {
         ['SortBy'] = 'job'--how to sort players on the list.
     }
 } -- example: 
@@ -24,8 +24,8 @@ VRConfig.ShowOnOffDuty = false -- true = show the list when a player is off duty
 
 VRConfig.ShowJob = true -- shows the player job grade in the list
 
-VRConfig.UpdateChannelEvent = 'srp-10system:server:updateChannel' -- your uptade channel event for updating the chanel in the list
+VRConfig.UpdateChannelEvent = 'vr-10system:server:updateChannel' -- your uptade channel event for updating the chanel in the list //YOU HAVE TO CHANGE IT!!!
 
-VRConfig.UpdateTalkingEvent = 'srp-10system:server:updateTalking' -- your uptade talking event for updating if you are talking
+VRConfig.UpdateTalkingEvent = 'vr-10system:server:updateTalking' -- your uptade talking event for updating if you are talking // YOU HAVE TO CHANGE IT!!!
 
 VRConfig.DefaultCodename = "None" -- Default code if the player didn't define one yet
